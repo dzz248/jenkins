c552ace9ef25f88e3dcf68ad16d296025dc166a0
duzizhen <424219722@qq.com>
2019-07-03 15:34:51 +0800

    '额提交以下'

---
+++ src/test/java/com/lesson/controller/OrderEntryControllorTest.java
@@ -0,0 +1,12 @@
+package com.lesson.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrderEntryControllorTest {
+
+    @Test
+    public void showMenus() {
+    }
+}
